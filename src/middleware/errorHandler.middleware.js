d5a64dc3ecdf4cbfe704f481d3c66063cb6d22c9
Mbaduko <nsengiyumvaclement247@gmail.com>
2024-10-19 18:54:07 +0200

    feat:DEfining Error handler middleware

---
+++ src/middleware/errorHandler.middleware.js
@@ -0,0 +1,16 @@
+const errorHandler = (err, req, res, next) => {
+
+    const { statusCode, message } = err; 
+    if (!statusCode) {
+        console.error(err.stack);
+        return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
+            success:false,
+            message:"Server error"
+        });
+    }
+
+    return res.status(statusCode).json({
+        success:false,
+        message
+    });
+}
