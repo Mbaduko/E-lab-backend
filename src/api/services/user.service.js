14187e65408ff4bf4ba2fd47799da73ab1b3601e
Mbaduko <nsengiyumvaclement247@gmail.com>
2024-10-20 09:22:41 +0200

    feat:Checking if user to be registered does not exists

--- src/api/services/user.service.js
+++ src/api/services/user.service.js
@@ -1,7 +1,25 @@
+import { Op } from 'sequelize';
 import db from '../models/index.js';
+import User from '../models/user.model.js';
+import AppError from '../../errors.js'
 
+const findUser = async (email, phone) => {
+    const user = await User.findOne({
+        where:{
+            [Op.or]: [
+                {email},
+                {phone}
+            ]
+        }
+    });;
+
+    return user;
+}
 
 const addUser = async (inputs) => {
+    const userExists = await findUser(inputs.email, inputs.phone);
+    if (userExists)
+        throw new AppError("User already registered", 400)
     return await  db.user.create(inputs);
 };
 
--- src/middleware/errorHandler.middleware.js
+++ src/middleware/errorHandler.middleware.js
@@ -3,13 +3,12 @@ import httpStatus from 'http-status';
 const errorHandler = (err, req, res, next) => {
     const { statusCode, message } = err; 
     if (!statusCode) {
-        console.log("Its me!!!!!!!!")
         console.error(err);
         return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
             success:false,
             message:"Server error"
         });
-    }i
+    }
 
     return res.status(statusCode).json({
         success:false,
